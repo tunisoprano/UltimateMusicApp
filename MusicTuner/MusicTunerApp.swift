2de1c76a8932958f0fb44177e6fc20658c476cf9
tunahansari <147708099+tunisoprano@users.noreply.github.com>
2026-01-06 02:32:22 +0300

    Initial Commit

---
+++ MusicTuner/MusicTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTunerApp.swift
+//  MusicTuner
+//
+//  Created by Tunahan Sarı on 6.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MusicTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicTunerTests/MusicTunerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTunerTests.swift
+//  MusicTunerTests
+//
+//  Created by Tunahan Sarı on 6.01.2026.
+//
+
+import Testing
+@testable import MusicTuner
+
+struct MusicTunerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
